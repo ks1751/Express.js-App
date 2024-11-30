d135a5cabcb64a291864e56e94b377d1f484cb38
ks1751 <ks1751@live.mdx.ac.uk>
2024-11-30 07:50:28 +0100

    logger

--- server.js
+++ server.js
@@ -10,3 +10,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.set('json spaces', 3);
+
+app.use((req, res, next) => {
+    const time = new Date().toISOString();
+    console.log(`[${time}] ${req.method} ${req.url} - IP: ${req.ip}`); 
+    if (req.body && Object.keys(req.body).length > 0) {
+        console.log('Request Body:', JSON.stringify(req.body, null, 2));
+    }
+    res.on('finish', () => {
+        console.log(`[${new Date().toISOString()}] Response Status: ${res.statusCode}`);
+        console.log('-'.repeat(50));
+    });
+    next();
+});
