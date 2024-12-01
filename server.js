a429404d47998e1acb6b3e37cd381745bdfd4747
ks1751 <ks1751@live.mdx.ac.uk>
2024-12-01 14:57:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile('/Users/mac/Documents/Vue.js App/index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const uri = "mongodb+srv://ks1751:Olaoluwa88@cluster0.kyiza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; // Replace with your MongoDB Atlas URI
