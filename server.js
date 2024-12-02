1b191ada25613a98325b0eda7841f559bce6f180
ks1751 <ks1751@live.mdx.ac.uk>
2024-12-02 07:28:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const {MongoClient, ObjectId} = require('mongodb');
 const cors = require('cors');
-const path = require('path');
-const fs = require('fs');
 const PORT = 3000;
-
 const app = express();
-
-
 
 app.use(cors());
 app.use(express.json());
